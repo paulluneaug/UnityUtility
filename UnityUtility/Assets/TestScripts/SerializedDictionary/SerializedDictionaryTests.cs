5ac342ed9a79805c2b23c52ef00421809a510364
p.luneau <p.luneau@acreos.eu>
2024-01-11 11:55:43 +0100

    Updated Test script

--- UnityUtility/Assets/TestScripts/SerializedDictionary/SerializedDictionaryTests.cs
+++ UnityUtility/Assets/TestScripts/SerializedDictionary/SerializedDictionaryTests.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityUtility.SerializedDictionary;
 
 public class SerializedDictionaryTests : MonoBehaviour
 {
