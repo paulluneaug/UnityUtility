b0a736f57bc7162f594771798e35ad2010912ed6
p.luneau <p.luneau@acreos.eu>
2024-02-20 13:09:45 +0100

    More recorder types

--- UnityUtility/Assets/Public/Runtime/Recorders/GenericRecorder.cs
+++ UnityUtility/Assets/Public/Runtime/Recorders/GenericRecorder.cs
@@ -1,48 +1,28 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-namespace UnityUtility.Recorder
+namespace UnityUtility.Recorders
 {
-    public class ScriptExecutionTimeRecorder
+    public abstract class GenericRecorder<TEvent> where TEvent : ScriptEvent
     {
-        protected readonly struct ScriptEvent
-        {
-            private readonly string m_eventName;
-            private readonly double m_eventDuration;
-            private readonly int m_frame;
-            private readonly string m_recorderName;
-
-            public ScriptEvent(string eventName, double eventDuration, string recorderName)
-            {
-                m_eventName = eventName;
-                m_eventDuration = eventDuration;
-                m_frame = Time.frameCount;
-                m_recorderName = recorderName;
-            }
-
-            public override readonly string ToString()
-            {
-                return $"[{m_recorderName}][Frame {m_frame}] Event \"{m_eventName}\" took {m_eventDuration} ms";
-            }
-        }
-
         protected const string DEFAULT_RECODER_NAME = "Recorder";
         protected const string RECODER_NAME_FMT = "Recorder {0}";
 
-        protected List<ScriptEvent> m_events = null;
+        public string Name => m_recorderName;
+
+        protected Stack<TEvent> m_events = null;
 
         protected DateTime m_firstEventTime;
         protected DateTime m_previousEventTime;
         protected readonly Action<string> m_logMethod;
         protected readonly string m_recorderName;
 
-        public ScriptExecutionTimeRecorder(string recorderName = "", LogType logType = LogType.Log)
+        public GenericRecorder(string recorderName = "", LogType logType = LogType.Log)
         {
             m_recorderName = string.IsNullOrEmpty(recorderName) ? DEFAULT_RECODER_NAME : string.Format(RECODER_NAME_FMT, recorderName);
 
-            m_events = new List<ScriptEvent>();
+            m_events = new Stack<TEvent>();
             ReinitTimes();
 
             m_logMethod = logType switch
@@ -58,47 +38,50 @@ namespace UnityUtility.Recorder
             };
         }
 
-        public virtual void AddEvent(string eventName, bool logImmediatly = true)
+        protected virtual void CreateEvent(string eventName, bool logImmediatly = true)
         {
             if (string.IsNullOrEmpty(eventName))
             {
-                Debug.LogError($"[Recorder] This event name is null or empty, it is not a valid name");
+                Debug.LogError(FormatLog("This event name is null or empty, it is not a valid name"));
                 return;
             }
-
-            DateTime now = DateTime.Now;
-            ScriptEvent newEvent = new ScriptEvent
+            TEvent newEvent = NewEvent
             (
-                eventName,
-                (now - m_previousEventTime).TotalMilliseconds,
-                m_recorderName
+                eventName
             );
-            m_events.Add(newEvent);
+            m_events.Push(newEvent);
 
             if (logImmediatly)
             {
                 m_logMethod(newEvent.ToString());
             }
 
-            m_previousEventTime = now;
+            m_previousEventTime = Now();
         }
+
+        protected void LogEvent(TEvent eventToLog)
+        {
+            m_logMethod(eventToLog.ToString());
+        }
+
+        protected abstract TEvent NewEvent(string eventName);
 
         public virtual void LogLastEvent()
         {
             if (m_events.Count > 0)
             {
-                m_logMethod(m_events.Last().ToString());
+                m_logMethod(m_events.Peek().ToString());
             }
             else
             {
-                m_logMethod($"[{m_recorderName}] No events to log");
+                m_logMethod(FormatLog("No events to log"));
             }
         }
 
         public virtual void LogAllEvents()
         {
             string log = string.Empty;
-            foreach (ScriptEvent e in m_events)
+            foreach (TEvent e in m_events)
             {
                 log += e.ToString() + "\n";
             }
@@ -120,13 +103,23 @@ namespace UnityUtility.Recorder
 
         protected virtual void ReinitTimes()
         {
-            m_firstEventTime = DateTime.Now;
+            m_firstEventTime = Now();
             m_previousEventTime = m_firstEventTime;
         }
 
         protected virtual string GetAllEventsTimeSpan()
         {
-            return $"[{m_recorderName}] All the logged events took {(m_previousEventTime - m_firstEventTime).TotalMilliseconds} ms so far";
+            return FormatLog($"All the logged events took {(m_previousEventTime - m_firstEventTime).TotalMilliseconds} ms so far");
+        }
+
+        protected virtual string FormatLog(string log)
+        {
+            return $"[{m_recorderName}] {log}";
+        }
+
+        protected static DateTime Now()
+        {
+            return DateTime.Now;
         }
     }
 }
