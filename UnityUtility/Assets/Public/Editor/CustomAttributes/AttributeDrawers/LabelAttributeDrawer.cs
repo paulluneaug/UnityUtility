3a34899a2af7d3b1670724a6ef1c9f09a6f94b40
p.luneau <p.luneau@acreos.eu>
2024-03-07 17:02:07 +0100

    Array suport for LabelAttribute

--- UnityUtility/Assets/Public/Editor/CustomAttributes/AttributeDrawers/LabelAttributeDrawer.cs
+++ UnityUtility/Assets/Public/Editor/CustomAttributes/AttributeDrawers/LabelAttributeDrawer.cs
@@ -3,30 +3,53 @@ using UnityEditor;
 using System;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using UnityUtility.Utils.Editor;
 
 namespace UnityUtility.CustomAttributes.Editor
 {
     [CustomPropertyDrawer(typeof(LabelAttribute))]
     public class LabelAttributeDrawer : PropertyDrawer
     {
-        private VisualElement m_drawnProperty;
+        private PropertyField m_drawnProperty;
+        private bool m_partOfArray;
 
-        #region VisualElement
+        #region UIElements
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
+            m_partOfArray = property.IsPropertyPartOfArray(out SerializedProperty arrayProperty, out int index);
             m_drawnProperty = new PropertyField(property);
-            m_drawnProperty.RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+            m_drawnProperty.name = "DrawnProperty";
+            if (!m_partOfArray || true)
+            {
+                m_drawnProperty.RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+            }
             return m_drawnProperty;
         }
 
         private void OnAttachToPanel(AttachToPanelEvent evt)
         {
+            //m_drawnProperty.UnregisterCallback<AttachToPanelEvent>(OnAttachToPanel);
             if (attribute is LabelAttribute labelAttribute)
             {
                 Label propertyLabel = m_drawnProperty.Q<Label>();
+                Foldout foldout = m_drawnProperty.Q<Foldout>();
+                Debug.LogError(m_drawnProperty.name);
+                foreach (var element in m_drawnProperty.Children())
+                {
+                    Debug.Log(element.name);
+                }
+                if (foldout != null)
+                {
+                    Debug.LogWarning(foldout.bindingPath);
+                }
+                if (propertyLabel == null)
+                {
+                    Debug.LogError($"No Label found for property {m_drawnProperty.bindingPath}");
+                    return;
+                }
                 if (!string.IsNullOrEmpty(labelAttribute.OverrideName))
                 {
-                    propertyLabel.text = labelAttribute.OverrideName;
+                    //propertyLabel.text = labelAttribute.OverrideName;
                 }
 
                 FontStyle labelStyle = FontStyle.Normal;
