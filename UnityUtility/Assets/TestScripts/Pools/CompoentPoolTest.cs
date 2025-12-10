c06325e9432347bcbff056c9637d83403e4d7e97
Paul Luneau <paul.luneau@mail.com>
2025-12-10 14:37:08 +0100

    Updated tests

--- UnityUtility/Assets/TestScripts/Pools/CompoentPoolTest.cs
+++ UnityUtility/Assets/TestScripts/Pools/CompoentPoolTest.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 
 using UnityEngine;
 
 using UnityUtility.Pools;
 
-public class CompoentPoolTest : ComponentPool<FieldsFiltersTest>
+public class CompoentPoolTest : MonoBehaviour
 {
+    [SerializeField] private FieldsFiltersTest m_elementPrefab;
+    [NonSerialized] private ComponentPool<FieldsFiltersTest> m_pool;
+
+    private void Awake()
+    {
+        m_pool = new ComponentPool<FieldsFiltersTest>(10, transform, m_elementPrefab);
+    }
+
     private void FixedUpdate()
     {
-        PooledObject<FieldsFiltersTest> a = Request();
+        PooledObject<FieldsFiltersTest> a = m_pool.Request();
         a.Object.gameObject.SetActive(true);
         _ = StartCoroutine(ReleaseAfter(a, 0.5f));
     }
